fb10a2808b1b2ebca2a89d2aa4bec7587320a3c7
spencer-norwick <norwick.spencer@gmail.com>
2026-06-12 13:43:03 -0700

    Harden daily automation PATH

--- scripts/librarian-daily.sh
+++ scripts/librarian-daily.sh
@@ -3,5 +3,6 @@ set -eu
 
 cd /Users/spencer/librarian
 export PYTHONPATH=src
+export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
 .venv/bin/python -m librarian daily --apply --lookup --enrich
